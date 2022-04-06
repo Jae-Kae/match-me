37524fbfd7ab64c78c2215078ee20aa48543b877
Janette King <janetteking96@gmail.com>
2022-04-06 14:46:52 -0700

    express server created

---
+++ server/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const morgan = require("morgan");
+
+
+const PORT = 4000;
+express()
+
+
+.listen(PORT, () => console.info(`Listening on port ${PORT}`));
